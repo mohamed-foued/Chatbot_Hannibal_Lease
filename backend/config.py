58b804193ac013e78bb46eaf6be7c346cf6a7131
mohamed-foued <mohamed-fouad.chaabane@eleve.ensai.fr>
2026-07-23 09:10:23 +0200

    fsv

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env")
+load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env", override=True)
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 DB_PATH = os.getenv("DB_PATH","leasing.db")
